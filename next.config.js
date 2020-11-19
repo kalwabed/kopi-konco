8568b8955882369a0721c8b87fce5f8e1652af76
kaliwa <49640654+kalwabed@users.noreply.github.com>
2020-11-19 13:51:30 +0700

    feat: add PWA feature

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+const withPwa = require('next-pwa')
+
+module.exports = withPwa({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public'
+  },
   images: {
     domains: ['images.unsplash.com', 'images.ctfassets.net']
   }
-}
+})
